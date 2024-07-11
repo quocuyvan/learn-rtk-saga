6befcc469fc455b9f0f39a1c72ed5e80d1861253
quocuyvan <quocuyvan1902@gmail.com>
2024-07-11 16:54:20 +0700

    Downgrade React 18 to 17; Add lib

--- src/models/common.ts
+++ src/models/common.ts
@@ -1,19 +1,19 @@
 export interface PaginationParams {
-  _limit: number;
-  _page: number;
-  _totalRows: number;
+    _limit: number;
+    _page: number;
+    _totalRows: number;
 }
 
 export interface ListResponse<T> {
-  data: T[];
-  pagination: PaginationParams;
+    data: T[];
+    pagination: PaginationParams;
 }
 
 export interface ListParams {
-  _page?: number;
-  _limit?: number;
-  _sort?: string;
-  _order?: 'asc' | 'desc';
+    _page?: number;
+    _limit?: number;
+    _sort?: string;
+    _order?: 'asc' | 'desc';
 
-  [key: string]: any;
+    [key: string]: any;
 }

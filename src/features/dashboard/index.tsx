f22bab3b5bdd8f8919ceeab923cb51c7dc42a0ff
quocuyvan <quocuyvan1902@gmail.com>
2024-07-12 18:00:21 +0700

    Setup saga for dashboard

--- src/features/dashboard/index.tsx
+++ src/features/dashboard/index.tsx
@@ -1,5 +1,33 @@
-import * as React from 'react';
+import { useAppDispatch, useAppSelector } from 'app/hooks';
+import {
+    dashboardActions,
+    selectDashboardLoading,
+    selectDashboardStatistics,
+    selectHightestStudentList,
+    selectLowestStudentList,
+    selectRankingByCityList,
+} from 'features/dashboard/dashboardSlice';
+import { useEffect } from 'react';
 
 export default function DashBoard() {
+    const dispatch = useAppDispatch();
+    const loading = useAppSelector(selectDashboardLoading);
+    const statistics = useAppSelector(selectDashboardStatistics);
+    const highestStudentList = useAppSelector(selectHightestStudentList);
+    const lowestStudentList = useAppSelector(selectLowestStudentList);
+    const rankingByCityList = useAppSelector(selectRankingByCityList);
+
+    console.log({
+        loading,
+        statistics,
+        highestStudentList,
+        lowestStudentList,
+        rankingByCityList,
+    });
+
+    useEffect(() => {
+        dispatch(dashboardActions.fetchData());
+    }, [dispatch]);
+
     return <div>Dashboard</div>;
 }

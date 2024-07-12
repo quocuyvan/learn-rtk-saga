59a3b5ea1d7c4c8c832d4ef84ac57f504d84127a
quocuyvan <quocuyvan1902@gmail.com>
2024-07-12 11:07:52 +0700

    Handle loading

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, makeStyles, Paper, Typography } from '@material-ui/core';
-import { useAppDispatch } from 'app/hooks';
+import { Box, Button, CircularProgress, makeStyles, Paper, Typography } from '@material-ui/core';
+import { useAppDispatch, useAppSelector } from 'app/hooks';
 import { authActions } from 'features/auth/authSlice';
 import * as React from 'react';
 
@@ -14,11 +14,15 @@ const useStyles = makeStyles((theme) => ({
     box: {
         padding: theme.spacing(3),
     },
+    mgr: {
+        marginRight: theme.spacing(1),
+    },
 }));
 
 export default function LoginPage() {
     const classes = useStyles();
     const dispatch = useAppDispatch();
+    const isLogging = useAppSelector((state) => state.auth.logging);
 
     const handleLoginClick = () => {
         dispatch(
@@ -42,6 +46,13 @@ export default function LoginPage() {
                         color={'primary'}
                         onClick={handleLoginClick}
                     >
+                        {isLogging && (
+                            <CircularProgress
+                                className={classes.mgr}
+                                size={20}
+                                color={'secondary'}
+                            />
+                        )}
                         Login
                     </Button>
                 </Box>
